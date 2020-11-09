b49727372421637ebb97565e1a8cec81703cc633
adityaharish218 <69881624+adityaharish218@users.noreply.github.com>
2020-11-09 15:10:26 +0800

    Coding day 1

--- Teachers/Teacher.java
+++ Teachers/Teacher.java
@@ -1,92 +1,11 @@
 package Teachers;
 
 public class Teacher {
-	private String name; //name of teacher
-	private int noOfLessonsinWeek; // no of Lessons in the day
-	private boolean headOfDepartment; //whether the teacher is head of department or not
-	private int hoursWorked; // no of hours they worked in the day
-	private boolean admin; //whether they are admin or not
-	private String meetingDay;
-	int points;
-	
-	public Teacher() {}
-	public Teacher(String name, int noOfLessonsinWeek, boolean headOfDepartement, int hoursWorked, boolean admin, String meetingDay) {
-		this.setName(name);
-		this.setNoOfLessonsinDay(noOfLessonsinWeek);
-		this.setHeadOfDepartment(headOfDepartement);
-		this.setHoursWorked(hoursWorked);
-		this.setAdmin(admin);
-		this.setMeetingDay(meetingDay);
-		setPoints();
-	}
-	public String getMeetingDay() {
-		return meetingDay;
-	}
-	public void setMeetingDay(String meetingDay) {
-		this.meetingDay = meetingDay;
-	}
-
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getNoOfLessonsinDay() {
-		return noOfLessonsinWeek;
-	}
-	public void setNoOfLessonsinDay(int noOfLessons) {
-		this.noOfLessonsinWeek = noOfLessons;
-	}
-	public boolean isHeadOfDepartment() {
-		return headOfDepartment;
-	}
-	public void setHeadOfDepartment(boolean headOfDepartment) {
-		this.headOfDepartment = headOfDepartment;
-	}
-	public int getHoursWorked() {
-		return hoursWorked;
-	}
-	public void setHoursWorked(int hoursWorked) {
-		this.hoursWorked = hoursWorked;
-	}
-	public boolean isAdmin() {
-		return admin;
-	}
-	public void setAdmin(boolean admin) {
-		this.admin = admin;
-	}
-	
-	public String isAdmintoString() {
-		if(this.admin) {
-			return "yes";
-		}
-		return "no";
-	}
-	public String isHeadOfDepartmenttoString() {
-		if(this.headOfDepartment) {
-			return "yes";
-		}
-		return "no";
-	}
-	
-	private void setPoints() {
-		if(this.admin) {
-			this.points = 0;
-			return;
-		}
-		
-	}
-	
-	
-	public String toString() {
-		String returningString = "Teacher Name : " + this.name + "\n";
-		returningString = returningString + "No of lessons in the week : " + this.noOfLessonsinWeek + "\n";
-		returningString = returningString + "Hours worked: " + this.hoursWorked + "\n";
-		returningString = returningString + "HeadofDepartment: " + this.isHeadOfDepartmenttoString() + "\n";
-		returningString = returningString + "Admin: " + this.isAdmintoString() + "\n";
-		return returningString;
-	}
-	
+	String name; //name of the teacher
+	int lessonsPerWeek; //number of lessons the teacher has per week
+	boolean admin; //whether the teacher is an administrator or not
+	int id; //id of the teacher
+	int dutiesToBeAssigned; // the number of duties that should be assigned to the teacher
+	int dutiesAssigned;// the number of duties currently done by the teacher
+	int points; // the number of points the teacher has. Based on conditions
 }
--- duties/Duty.java
+++ duties/Duty.java
@@ -1,37 +1,9 @@
 package duties;
 
 public class Duty {
-	private String location; //where the duty is, ex atrium, 8th floor
-	private Time time; // when the duty is ex 8:00, 12:00
-	int pointsDeducted;
-	
-	public Duty() {} //empty constructor
-	
-	public Duty(String location, Time t) { // constructor with Time
-		this.setLocation(location);
-		this.setTime(t.getHours(), t.getMinutes());
-	}
-	
-	public Duty (String location, int hours, int minutes) { // constructor with hours and minutes
-		this.setLocation(location);
-		this.setTime(hours, minutes);
-	}
-	public String getLocation() { //return the location
-		return location;
-	}
-	public void setLocation(String location) { // set the location
-		this.location = location;
-	}
-	public String getTime() { //return the time in a string
-		return time.toString();
-	}
-	public void setTime(int hours, int minutes) { // set the time
-		time.setTime(hours, minutes);
-	}
-	
-	public String toString() { //return the duty in a string
-		return "Location of Duty: " + this.location + " Time of duty: " + this.time;
-	}
-	
-	
+	String name; //name of duty
+	Time startTime; //Start time of duty
+	Time endTime; //End time of duty
+	String dayOfTheWeek; //day of the week the duty is
+	int pointsToDeduct; //points to deduct from teacher if duty is assigned
 }
